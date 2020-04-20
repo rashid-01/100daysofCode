bacc0d06172d73cc299779a224eb094f52975b47
rashid-01 <mdrashid.1000@gmail.com>
2020-04-20 23:15:58 +0530

    changes

--- wherefore-art-thou.js
+++ wherefore-art-thou.js
@@ -1,6 +1,15 @@
-function telephoneCheck(str) {
-	var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-	return regex.test(str);
+function whatIsInAName(collection, source) {
+	var srcKeys = Object.keys(source);
+	// Only change code below this line
+	return collection.filter(function(obj) {
+		return srcKeys.every(function(key) {
+			return obj.hasOwnProperty(key) && obj[key] === source[key];
+		});
+	});
+	// Only change code above this line
 }
 
-telephoneCheck('555-555-5555');
+whatIsInAName(
+	[ { first: 'Romeo', last: 'Montague' }, { first: 'Mercutio', last: null }, { first: 'Tybalt', last: 'Capulet' } ],
+	{ last: 'Capulet' }
+);
