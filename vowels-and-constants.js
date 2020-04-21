1bc67c1778e81319a1513b42495ec15937eca842
rashid-01 <mdrashid.1000@gmail.com>
2020-04-21 14:21:28 +0530

    New exercises

---
+++ vowels-and-constants.js
@@ -0,0 +1,9 @@
+function vowelsAndConsonants(s) {
+	let vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+	for (let v of s) {
+		if (vowels.includes(v)) console.log(v);
+	}
+	for (let v of s) {
+		if (!vowels.includes(v)) console.log(v);
+	}
+}
